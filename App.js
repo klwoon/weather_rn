3f91221d443f6ff4740d96a297aaaf2d214ceed4
Woon <klwoon1980@yahoo.com>
2019-03-23 18:29:54 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, Text, View, Platform, KeyboardAvoidingView, TextInput } from 'react-native';
+
+import SearchInput from './components/SearchInput';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <KeyboardAvoidingView 
+        style={styles.container}
+        behavior="padding"
+      >
+        <Text style={[styles.largeText, styles.textStyle]}>
+          San Franciso
+        </Text>
+        <Text style={[styles.smallText, styles.textStyle]}>
+          Light Cloud
+        </Text>
+        <Text styles={[styles.largeText, styles.textStyle]}>
+          24°
+        </Text>
+
+        <SearchInput placeholder="Search any city" />
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular': 'Roboto',
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+  },  
+});
